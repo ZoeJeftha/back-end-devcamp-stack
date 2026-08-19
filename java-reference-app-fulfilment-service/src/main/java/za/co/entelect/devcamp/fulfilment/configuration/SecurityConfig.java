8273304ed90a11cc4a3a3b76dfc25d75191279ea
zonisha.mahomed <jeftha.z@gmail.com>
2026-08-19 16:31:10 +0200

    Add fulfilment service

---
+++ java-reference-app-fulfilment-service/src/main/java/za/co/entelect/devcamp/fulfilment/configuration/SecurityConfig.java
@@ -0,0 +1,54 @@
+package za.co.entelect.devcamp.fulfilment.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${pub.key}")
+    private RSAPublicKey publicKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests(requests -> requests
+                        .antMatchers("/health", "/info", "/metrics", "/v1/stuff").permitAll()
+                        .anyRequest().authenticated())
+                .csrf().disable()
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                )
+                .oauth2ResourceServer()
+                .jwt()
+                .and()
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
+    }
+}
---
+++ java-reference-app-fulfilment-service/src/main/java/za/co/entelect/devcamp/fulfilment/controller/FulfilmentController.java
@@ -0,0 +1,34 @@
+package za.co.entelect.devcamp.fulfilment.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.Instant;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1")
+public class FulfilmentController {
+
+    public FulfilmentController()
+    {
+    }
+
+    @GetMapping("/stuff")
+    public String getStuff()
+    {
+        log.info("******** THIS IS MY NEW FULFILMENT SERVICE 2********");
+        return "TEST 123";
+    }
+}
