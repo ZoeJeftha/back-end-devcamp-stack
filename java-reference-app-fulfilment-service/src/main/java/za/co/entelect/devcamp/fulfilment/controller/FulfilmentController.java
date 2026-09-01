c02603e475aa19941bb85f1040f8ccc8b58f8db7
zonisha.mahomed <jeftha.z@gmail.com>
2026-09-01 15:30:00 +0200

    Do Credit Checks

--- java-reference-app-fulfilment-service/src/main/java/za/co/entelect/devcamp/fulfilment/controller/FulfilmentController.java
+++ java-reference-app-fulfilment-service/src/main/java/za/co/entelect/devcamp/fulfilment/controller/FulfilmentController.java
@@ -1,22 +1,36 @@
 package za.co.entelect.devcamp.fulfilment.controller;
 
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import za.co.entelect.devcamp.fulfilment.interfaces.ICreditChecksApiClient;
 
 @Slf4j
 @RestController
 @RequestMapping("/v1")
 public class FulfilmentController {
 
-    public FulfilmentController()
+    public final ICreditChecksApiClient creditChecksApiClient;
+
+    public FulfilmentController(ICreditChecksApiClient creditChecksApiClient)
     {
+        this.creditChecksApiClient = creditChecksApiClient;
     }
 
     @GetMapping("/stuff")
-    public String getProducts()
+    public String DoCreditCheck()
     {
-        return "Temp";
+        try {
+            return creditChecksApiClient.DoCreditCheck(1L);
+        }
+        catch (IOException e) {
+            return "IOException thrown: "+ e.getMessage();
+        }
+        catch(Exception e)
+        {
+            return "Exception thrown: "+ e.getMessage();
+        }
     }
 }
